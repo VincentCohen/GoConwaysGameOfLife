dee970d89c5044c0a84805008c36c3a8bb67d3e1
Vincent <vincent@getseoshop.com>
2018-04-13 16:19:33 +0200

    Colors & grid. Saving the generation

--- main.go
+++ main.go
@@ -8,36 +8,54 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/fatih/color"
 )
 
-// ┌http://www.theasciicode.com.ar/extended-ascii-code/box-drawing-character-single-line-upper-left-corner-ascii-code-218.html
+//http://www.theasciicode.com.ar/extended-ascii-code/box-drawing-character-single-line-upper-left-corner-ascii-code-218.html
 func main() {
 	height, width := grid()
-
+	width = width - 2
 	height = height - 3 // minus top, minus bottom, minus enter
 
-	fmt.Println(width)
-	fmt.Println(height)
-
 	var buffer bytes.Buffer
+
+	c := map[int]map[int]bool{}
+	c[0] = map[int]bool{}
+
+	c[0][0] = true
+	c[0][1] = true
+	c[0][2] = true
+	c[0][3] = true
+	c[0][4] = true
+
+	c[1] = map[int]bool{}
+	c[1][4] = true
+	c[1][5] = true
+	c[1][6] = true
+	c[1][7] = true
+	c[1][8] = true
 
 	// for cont := true; cont; cont = true {
 	// Draw top, start at 2 for the corners
 	buffer = drawGridTop(buffer, width)
-	buffer = drawCenter(buffer, width, height)
+	buffer = drawCenter(buffer, width, height, c)
 	buffer = drawGridBottom(buffer, width)
 
-	fmt.Println(buffer.String())
+	// fmt.Println(buffer.String())
+	for i := 0; i < 1000; i++ {
+		clear := exec.Command("clear")
+		clear.Stdout = os.Stdout
+		clear.Run()
 
-	// clear := exec.Command("clear")
-	// clear.Stdout = os.Stdout
-	// clear.Run()
-	// }
+		time.Sleep(time.Second / time.Duration(120))
+
+		fmt.Println(buffer.String())
+	}
 }
 
-func grid() (uint, uint) {
+func grid() (int, int) {
 	cmd := exec.Command("stty", "size")
 	cmd.Stdin = os.Stdin
 	out, err := cmd.Output()
@@ -46,18 +64,16 @@ func grid() (uint, uint) {
 	}
 	parts := strings.Split(string(out), " ")
 
-	fmt.Println(parts)
-
 	x, err := strconv.Atoi(parts[0])
 
 	y, err := strconv.Atoi(strings.Replace(parts[1], "\n", "", 1))
 
-	return uint(x), uint(y)
+	return int(x), int(y)
 }
 
-func drawGridTop(b bytes.Buffer, width uint) bytes.Buffer {
+func drawGridTop(b bytes.Buffer, width int) bytes.Buffer {
 	b.WriteString("┌")
-	for i := uint(2); i < width; i++ {
+	for i := int(0); i < width; i++ {
 		b.WriteString("─")
 	}
 	b.WriteString("┐\n")
@@ -65,22 +81,40 @@ func drawGridTop(b bytes.Buffer, width uint) bytes.Buffer {
 	return b
 }
 
-func drawCenter(b bytes.Buffer, width uint, height uint) bytes.Buffer {
+func drawCenter(b bytes.Buffer, width int, height int, generation map[int]map[int]bool) bytes.Buffer {
 	// Draw in between
-	// var death = "▒"
-	// go get github.com/fatih/color
-	var death = color.HiBlackString("▓")
-	var life = color.HiGreenString("▓") //"▓"
+	var death = color.HiBlackString("0")
+	var life = color.HiGreenString("1")
 
-	for i := uint(0); i < height; i++ {
+	for x := int(0); x < height; x++ {
 		b.WriteString("│")
 
-		for j := uint(2); j < width; j++ {
-			if (j % 12) == 0 {
+		for y := int(0); y < width; y++ {
+			// Generate the playing field for the first go
+			if generation[x][y] {
 				b.WriteString(life)
 			} else {
+				// Decide the normal live / death states as they are advancing generation
 				b.WriteString(death)
 			}
+
+			// Track states
+
+			// Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+			// Live cell < 2 live = dead
+
+			// Any live cell with two or three live neighbours lives on to the next generation.
+			// Live cell with  >2 OR 3  live neighbours = next generation
+
+			// Any live cell with more than three live neighbours dies, as if by overpopulation.
+			// Live cell with >3 live neighbours = dead
+
+			// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+			// Dead cell with == 3 live neighbours = LIVE
+
+			// Define
+			// b.WriteString(life)
+
 		}
 		b.WriteString("│")
 		b.WriteString("\n")
@@ -89,9 +123,9 @@ func drawCenter(b bytes.Buffer, width uint, height uint) bytes.Buffer {
 	return b
 }
 
-func drawGridBottom(b bytes.Buffer, width uint) bytes.Buffer {
+func drawGridBottom(b bytes.Buffer, width int) bytes.Buffer {
 	b.WriteString("└")
-	for i := uint(2); i < width; i++ {
+	for i := int(0); i < width; i++ {
 		b.WriteString("─")
 	}
 	b.WriteString("┘")
