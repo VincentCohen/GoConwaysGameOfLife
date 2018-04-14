7945dfc3984da68de2ef99ac8b552c7dc66d8d65
Vincent Cohen <v.cohen90@gmail.com>
2018-04-14 23:00:34 +0200

    refactored

--- main.go
+++ main.go
@@ -15,12 +15,106 @@ import (
 
 //http://www.theasciicode.com.ar/extended-ascii-code/box-drawing-character-single-line-upper-left-corner-ascii-code-218.html
 func main() {
-	height, width := grid()
-	width = width - 2
-	height = height - 3 // minus top, minus bottom, minus enter
+	height, width := dimensions()
 
 	var buffer bytes.Buffer
 
+	generation := getFirstGeneration()
+
+	// Contains the output
+	buffer = top(buffer, width)
+	buffer, generation = nextGeneration(buffer, width, height, generation)
+	buffer = bottom(buffer, width)
+
+	// Should loop trough the generations
+	NextGeneration := true
+	for loop := true; loop; loop = NextGeneration {
+
+		clear := exec.Command("clear")
+		clear.Stdout = os.Stdout
+		clear.Run()
+
+		fmt.Println(buffer.String())
+
+		time.Sleep(time.Second / time.Duration(5))
+	}
+}
+
+/**
+Determines the dimensions of the game (terminal width/height)
+*/
+func dimensions() (int, int) {
+	cmd := exec.Command("stty", "size")
+	cmd.Stdin = os.Stdin
+	out, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	parts := strings.Split(string(out), " ")
+
+	x, err := strconv.Atoi(parts[0])
+
+	y, err := strconv.Atoi(strings.Replace(parts[1], "\n", "", 1))
+
+	return int(x - 2), int(y - 3) // Minus the sides, Minus the top
+}
+
+/**
+Draws the top of the game
+*/
+func top(b bytes.Buffer, width int) bytes.Buffer {
+	b.WriteString("┌")
+	for i := int(0); i < width; i++ {
+		b.WriteString("─")
+	}
+	b.WriteString("┐\n")
+
+	return b
+}
+
+/**
+Draws the next generation
+*/
+func nextGeneration(b bytes.Buffer, width int, height int, generation map[int]map[int]bool) (bytes.Buffer, map[int]map[int]bool) {
+	// Draw in between
+	var death = color.HiBlackString("0")
+	var life = color.HiGreenString("1")
+
+	for x := int(0); x < height; x++ {
+		b.WriteString("│")
+
+		for y := int(0); y < width; y++ {
+			// Retrieve the cell state
+			if populate(generation, x, y) {
+				b.WriteString(life)
+			} else {
+				b.WriteString(death)
+			}
+		}
+		b.WriteString("│")
+		b.WriteString("\n")
+	}
+
+	return b, generation
+}
+
+/**
+Draws the bottom of the game
+*/
+func bottom(b bytes.Buffer, width int) bytes.Buffer {
+	b.WriteString("└")
+	for i := int(0); i < width; i++ {
+		b.WriteString("─")
+	}
+	b.WriteString("┘")
+
+	return b
+}
+
+/**
+First generation
+*/
+func getFirstGeneration() map[int]map[int]bool {
 	c := map[int]map[int]bool{}
 	c[0] = map[int]bool{}
 
@@ -37,114 +131,32 @@ func main() {
 	c[1][7] = true
 	c[1][8] = true
 
-	// for cont := true; cont; cont = true {
-	// Draw top, start at 2 for the corners
-	buffer = drawGridTop(buffer, width)
-	buffer, c = drawCenter(buffer, width, height, c)
-	buffer = drawGridBottom(buffer, width)
-
-	NextGeneration := true
-	for loop := true; loop; loop = NextGeneration {
-
-		clear := exec.Command("clear")
-		clear.Stdout = os.Stdout
-		clear.Run()
-
-		fmt.Println(buffer.String())
-
-		time.Sleep(time.Second / time.Duration(5))
-	}
-
-	// // fmt.Println(buffer.String())
-	// for i := 0; i < 1000; i++ {
-	// 	time.Sleep(time.Second / time.Duration(1))
-
-	// 	clear := exec.Command("clear")
-	// 	clear.Stdout = os.Stdout
-	// 	clear.Run()
-
-	// 	buffer = drawGridTop(buffer, width)
-	// 	buffer, c = drawCenter(buffer, width, height, c)
-	// 	buffer = drawGridBottom(buffer, width)
-
-	// 	fmt.Println(buffer.String())
-	// }
+	return c
 }
 
-func grid() (int, int) {
-	cmd := exec.Command("stty", "size")
-	cmd.Stdin = os.Stdin
-	out, err := cmd.Output()
-	if err != nil {
-		log.Fatal(err)
+/**
+Determines wether the cells are alive or death based on the current generation
+*/
+func populate(generation map[int]map[int]bool, x int, y int) bool {
+
+	// Generate the playing field for the first go
+	if generation[x][y] {
+		return true
 	}
-	parts := strings.Split(string(out), " ")
+	return false
+	// Decide the normal live / death states as they are advancing generation
+	// Track states
 
-	x, err := strconv.Atoi(parts[0])
+	// Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+	// Live cell < 2 live = dead
 
-	y, err := strconv.Atoi(strings.Replace(parts[1], "\n", "", 1))
+	// Any live cell with two or three live neighbours lives on to the next generation.
+	// Live cell with  >2 OR 3  live neighbours = next generation
 
-	return int(x), int(y)
-}
+	// Any live cell with more than three live neighbours dies, as if by overpopulation.
+	// Live cell with >3 live neighbours = dead
 
-func drawGridTop(b bytes.Buffer, width int) bytes.Buffer {
-	b.WriteString("┌")
-	for i := int(0); i < width; i++ {
-		b.WriteString("─")
-	}
-	b.WriteString("┐\n")
+	// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+	// Dead cell with == 3 live neighbours = LIVE
 
-	return b
-}
-
-func drawCenter(b bytes.Buffer, width int, height int, generation map[int]map[int]bool) (bytes.Buffer, map[int]map[int]bool) {
-	// Draw in between
-	var death = color.HiBlackString("0")
-	var life = color.HiGreenString("1")
-
-	for x := int(0); x < height; x++ {
-		b.WriteString("│")
-
-		for y := int(0); y < width; y++ {
-			// Generate the playing field for the first go
-			if generation[x][y] {
-				b.WriteString(life)
-			} else {
-				// Decide the normal live / death states as they are advancing generation
-				b.WriteString(death)
-			}
-
-			// Track states
-
-			// Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
-			// Live cell < 2 live = dead
-
-			// Any live cell with two or three live neighbours lives on to the next generation.
-			// Live cell with  >2 OR 3  live neighbours = next generation
-
-			// Any live cell with more than three live neighbours dies, as if by overpopulation.
-			// Live cell with >3 live neighbours = dead
-
-			// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-			// Dead cell with == 3 live neighbours = LIVE
-
-			// Define
-			// b.WriteString(life)
-
-		}
-		b.WriteString("│")
-		b.WriteString("\n")
-	}
-
-	return b, generation
-}
-
-func drawGridBottom(b bytes.Buffer, width int) bytes.Buffer {
-	b.WriteString("└")
-	for i := int(0); i < width; i++ {
-		b.WriteString("─")
-	}
-	b.WriteString("┘")
-
-	return b
 }
